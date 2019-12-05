4934b9986e8f00faca9b5cde9f91c06ef5c7212b
Rasul Kireev <kireevr1996@gmail.com>
2019-12-05 10:56:55 -0500

    finalized v1 design

--- projects/models.py
+++ projects/models.py
@@ -4,19 +4,24 @@ from autoslug import AutoSlugField
 from taggit.managers import TaggableManager
 
 class Project(models.Model):
+    # Required Information
     website_title = models.CharField(max_length=100, unique=True)
     website_url = models.URLField(unique=True)
-    website_description = models.TextField()
+    website_short_description = models.CharField(max_length=200)
     user_email = models.EmailField()
     slug = AutoSlugField(populate_from='website_title')
     published = models.BooleanField(default=False)
     tags = TaggableManager(blank=True)
     
+    # Optional Website Information
+    website_description = models.TextField(blank=True)
     website_homepage_screenshot = models.ImageField(upload_to='website_homepage_screenshot/', blank=True)
     website_twitter = models.URLField(blank=True)
     website_github = models.URLField(blank=True)
     website_additional_info = models.TextField(blank=True)
+    website_requirements = models.TextField(blank=True)
     
+    # Optional Author Information
     author_name = models.CharField(max_length=100, blank=True)
     author_email = models.EmailField(blank=True)
     author_website = models.URLField(blank=True)
