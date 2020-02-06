cc8c31ecdf2205e6c3e864975f0c71b92f742808
Rasul Kireev <me@rasulkireev.com>
2020-02-06 16:20:10 -0500

    Redesigned the home page

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from .views import ProjectListView, ProjectCreateView, Thanks
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='home'),
-    path('project/new/', ProjectCreateView.as_view(), name='submit_project'),
+    path('project/new/', ProjectCreateView.as_view(), name='submit-project'),
     path('thanks/', Thanks.as_view(), name='sucessfull-submit'),
 ]
