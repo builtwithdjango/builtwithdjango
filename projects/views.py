ddeeeb2142a00f2af2be0294d8c28c092f20d368
Rasul Kireev <me@rasulkireev.com>
2020-02-13 09:43:46 -0500

    Fixed the submit success error. Added success message

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import Project
 from django.urls import reverse_lazy
 from .forms import AddProject
@@ -10,11 +11,13 @@ class ProjectListView(ListView):
     template_name = 'home.html'
     ordering = ['-date_added']
     
-class ProjectCreateView(CreateView):
+class ProjectCreateView(SuccessMessageMixin, CreateView):
     model = Project
     form_class = AddProject
-    template_name = 'submit_project.html'
-    success_url = reverse_lazy('sucessfull-submit')
+    template_name = 'submit-project.html'
+    success_url = reverse_lazy('home')
+    success_message = "Thanks for submitting your project! I'll let you know when it is up on the site!"
+
 
 class Thanks(TemplateView):
     template_name = "sucessfull-submit.html"
