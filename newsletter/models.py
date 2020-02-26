ae5bf4dcb3258fc07b52065d788831f534eb48eb
Rasul Kireev <me@rasulkireev.com>
2020-02-26 18:07:29 -0500

    Stared to attempt to integrate Email Octopus to Newsletter

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+import requests
 
 class Emails(models.Model):
     # Required Information
@@ -7,5 +9,13 @@ class Emails(models.Model):
 
     def __str__(self):
         return str(self.user_email)
+
+    def send_email_to_octopus(self):
+        emailoctopus_api_key = settings.EMAILOCTOPUS_API
+        list_id = "5ba12ae4-58c6-11ea-a3d0-06b4694bee2a"
+
+        return requests.post(f"https://emailoctopus.com/api/1.5/lists/{list_id}/contacts?api_key={emailoctopus_api_key}",
+                    data={"email_address":self.user_email})
+
 
 
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,15 +1,19 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+from django.conf import settings
+from django.contrib import messages
+from django.shortcuts import render
+
 from .models import Emails
 from .forms import NewsletterSignupForm
-from django.urls import reverse_lazy
 
-
-class NewsletterSignupView(CreateView):
+class NewsletterSignupView(SuccessMessageMixin, CreateView):
     template_name = "newsletter/email-form.html"
     form_class = NewsletterSignupForm
     model = Emails
-    success_url = reverse_lazy('newsletter-thanks')
-    
+    success_url = reverse_lazy('home')
+    success_message = "Thanks for subscribing!"
 
 class NewsletterThanksView(TemplateView):
     template_name = "newsletter/sucessfull-email-submit.html"
