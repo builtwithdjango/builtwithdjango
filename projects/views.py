f5bd23a523d19f19ee2786eecb1ab0b3f7defdc1
Rasul Kireev <kireevr1996@gmail.com>
2020-02-27 07:44:46 -0500

    EmailOctopus Forms are working now

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,11 @@
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, CreateView
 from django.views.generic.edit import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 from .models import Project
 from django.urls import reverse_lazy
+
+from newsletter.views import NewsletterSignupForm
+from newsletter.models import Emails
 from .forms import AddProject
 
 
@@ -10,6 +13,34 @@ class ProjectListView(ListView):
     model = Project
     template_name = 'home.html'
     ordering = ['-date_added']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = NewsletterSignupForm
+
+        return context
+
+class EmailFormView(SuccessMessageMixin, CreateView):
+    form_class = NewsletterSignupForm
+    model = Emails
+
+    def form_valid(self, form):
+        self.object = form.save()
+        
+        emailoctopus_api_key = settings.EMAILOCTOPUS_API
+        list_id = settings.OCTO_LIST_ID
+
+        data = {
+            "api_key": emailoctopus_api_key,
+            "email_address": self.object.user_email
+        }
+
+        response = requests.post(f"https://emailoctopus.com/api/1.5/lists/{list_id}/contacts", data=data)
+        response.status_code
+        messages.success(self.request, 'Thanks for signing up!')
+        
+        return HttpResponseRedirect(reverse_lazy('home'))
+
     
 class ProjectCreateView(SuccessMessageMixin, CreateView):
     model = Project
@@ -17,7 +48,6 @@ class ProjectCreateView(SuccessMessageMixin, CreateView):
     template_name = 'submit-project.html'
     success_url = reverse_lazy('home')
     success_message = "Thanks for submitting your project! I'll let you know when it is up on the site!"
-
 
 class Thanks(TemplateView):
     template_name = "sucessfull-submit.html"
