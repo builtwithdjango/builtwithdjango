4fcbdc7745a9e00848199080b0660c2d998c6996
Rasul Kireev <me@rasulkireev.com>
2020-07-29 15:47:47 -0400

    Added first test

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+
+class ProjectTestCase(TestCase):
+    def setUp(self):
+        Project.objects.create(
+            website_title="Test Site 1",
+            website_url="https://test.com",
+            website_short_description="This is fake test website",
+            user_email="test1@test.com",
+        )
+        Project.objects.create(
+            website_title="Example Site 1",
+            website_url="https://example.com",
+            website_short_description="This is fake example website",
+            user_email="example1@example.com",
+        )
+
+    def test_Projects_can_speak(self):
+        """Projects that can speak are correctly identified"""
+        test = Project.objects.get(website_title="Test Site 1")
+        example = Project.objects.get(website_title="Example Site 1")
+        self.assertEqual(test.user_email, "test1@test.com")
+        self.assertEqual(example.user_email, "example1@example.com")
