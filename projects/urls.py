c0cd4cbb752318e6eac242004ad7f334ab26a92a
Rasul Kireev <me@rasulkireev.com>
2020-09-12 11:48:13 -0400

    removed unsresolved imports

--- projects/urls.py
+++ projects/urls.py
@@ -3,15 +3,10 @@ from .views import (
     ProjectListView, 
     ProjectDetailView, 
     ProjectCreateView,
-    ProjectsAPIView,
-    MakersAPIView
 )
 
 urlpatterns = [
     path("", ProjectListView.as_view(), name="home"),
     path("<slug:slug>", ProjectDetailView.as_view(), name="project"),
     path("project/new/", ProjectCreateView.as_view(), name="submit-project"),
-
-    path('projects-list/', ProjectsAPIView.as_view(), name='projects-json'),
-    path('makers-list/', MakersAPIView.as_view(), name='makers-json'),
 ]
