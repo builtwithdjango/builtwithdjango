4c87330d4c5eaf4165b1f57f8a4a53e58933c361
Rasul Kireev <rasul.kireev@guycarp.com>
2021-09-14 22:01:43 -0400

    added a couple of fields, plus admin

--- pages/admin.py
+++ pages/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import CistercianDateNftRequest
 
-admin.site.register(CistercianDateNftRequest)
+
+class CistercianDateNfttAdmin(admin.ModelAdmin):
+    list_display = ["email", "wallet_public_key", "date_requested", "sent"]
+
+
+admin.site.register(CistercianDateNftRequest, CistercianDateNfttAdmin)
--- pages/models.py
+++ pages/models.py
@@ -7,6 +7,8 @@ class CistercianDateNftRequest(TimeStampedModel):
     wallet_public_key = models.CharField(max_length=200, unique=True)
     date_requested = models.DateField(unique=True)
     sent = models.BooleanField(default=False)
+    proof = models.URLField(blank=True)
+    link = models.URLField(blank=True)
 
     def __str__(self):
         return f"{self.email}-{self.date_requested}"
