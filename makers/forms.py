dc39aad0c9e6e0e0e5d479a27ad87f96e7211ae0
Rasul Kireev <rasul.kireev@guycarp.com>
2021-10-28 12:01:42 -0400

    added email notification when someone trie to claim account

--- makers/forms.py
+++ makers/forms.py
@@ -1,9 +1,30 @@
+from django.core.mail import send_mail
+from django.db import transaction
 from django.forms import ModelForm
 
 from .models import Maker
 
 
 class ClaimAccountForm(ModelForm):
+    def save(self):
+        instance = super(ClaimAccountForm, self).save()
+
+        @transaction.on_commit
+        def send_notification():
+            message = f"""
+          Someone tried claiming the account.
+          Instance: {instance}
+        """
+            send_mail(
+                "Account Claimed",
+                message,
+                "rasul@builtwithdjango.com",
+                ["rasul@builtwithdjango.com"],
+                fail_silently=False,
+            )
+
+        return instance
+
     class Meta:
         model = Maker
         fields = ("user",)
