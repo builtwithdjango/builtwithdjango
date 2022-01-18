544f326ff40aaf0097fbe930107d478e778d6587
Rasul Kireev <rasul.kireev@guycarp.com>
2022-01-18 15:26:52 -0500

    improved admin view for the users

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
 from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
+    list_display = ["date_joined", "username", "email", "first_name", "last_name"]
     model = CustomUser
 
-    fieldsets = UserAdmin.fieldsets + (
-        ("Extra Fields", {"fields": ("profile_image",)}),
-    )
+    fieldsets = UserAdmin.fieldsets + (("Extra Fields", {"fields": ("profile_image",)}),)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
