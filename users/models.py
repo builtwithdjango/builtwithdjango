84ad3ac4883a59e26e2478b5e2976eab2f0ce70f
Rasul Kireev <rasul.kireev@guycarp.com>
2022-02-21 12:27:46 -0500

    updated user model and admin display

--- users/models.py
+++ users/models.py
@@ -8,6 +8,18 @@ from django.db import models
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=20, blank=True)
     last_name = models.CharField(max_length=20, blank=True)
+    slug = AutoSlugField(populate_from="username", always_update=True, editable=True)
+    interviewed = models.BooleanField(default=True)
+
+    # Social
+    twitter_handle = models.CharField(max_length=20, blank=True)
+    github_handle = models.CharField(max_length=20, blank=True)
+    indiehackers_handle = models.CharField(max_length=20, blank=True)
+    personal_website = models.URLField(blank=True)
+
+    # Additional
+    interviewed = models.BooleanField(default=False)
+    short_bio = models.TextField(blank=True)
 
     referred_by = models.OneToOneField(
         settings.AUTH_USER_MODEL,
