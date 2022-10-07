86885a1fa653100332bdd86793303a72bd657da2
Rasul Kireev <rasul.kireev@guycarp.com>
2022-10-07 18:02:29 -0400

    rmeoving some print statements

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -6,15 +6,10 @@ from django.conf import settings
 
 def add_email_to_revue(email, double_opt_in=True):
     data = {"email": str(email), "double_opt_in": double_opt_in}
-
-    print(f"Adding {email} to Revue")
     r = requests.post(
         f"https://www.getrevue.co/api/v2/subscribers",
         headers={"Authorization": f"Token {settings.REVUE_API_TOKEN}"},
         json=data,
     )
-    print(f"Done")
-    print(f"Status: {r.status_code}")
-    print(f"JSON: {r.json()}")
 
     return r.json()
