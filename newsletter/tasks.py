a7d3ab5ae24cf555890ebc4e543c1ba546ac02ca
Rasul Kireev <rasul.kireev@guycarp.com>
2022-10-07 16:36:45 -0400

    fix the name of var

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -5,6 +5,6 @@ from django.conf import settings
 def add_email_to_revue(instance):
     requests.post(
         f"https://www.getrevue.co/api/v2/subscribers",
-        headers={"Authorization": f"Token {settings.REVUE_API_KEY}"},
+        headers={"Authorization": f"Token {settings.REVUE_API_TOKEN}"},
         data={"email": instance.user_email},
     )
