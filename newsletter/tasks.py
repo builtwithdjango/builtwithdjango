959e77ecbdcfb2df4dd2125e65339bffad2accdc
Rasul Kireev <rasul.kireev@guycarp.com>
2023-02-08 17:31:31 -0500

    mark user as regular if he is subscribed user

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -9,7 +9,11 @@ def add_email_to_buttondown(email, tag):
         "email": str(email),
         "metadata": {"source": tag},
         "tags": [tag],
+        "referrer_url": "https://builtwithdjango.com",
     }
+    if tag == "user":
+        data["subscriber_type"] = "regular"
+
     r = requests.post(
         f"https://api.buttondown.email/v1/subscribers",
         headers={"Authorization": f"Token {settings.BUTTONDOWN_API_TOKEN}"},
