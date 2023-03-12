fe4c7aaa16707e154627f5fe0d831f6f0bdc7565
Rasul Kireev <rasul.kireev@guycarp.com>
2023-03-12 12:15:24 -0400

    remove old database call

--- builtwithdjango/settings.py
+++ builtwithdjango/settings.py
@@ -124,13 +124,6 @@ WSGI_APPLICATION = "builtwithdjango.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
-
 DATABASES = {
     "default": env.db_url(),
 }
