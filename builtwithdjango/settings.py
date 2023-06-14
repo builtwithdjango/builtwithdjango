fc4aecd1f015cf1e81f369c24920300660543c0d
Rasul Kireev <rasul.kireev@guycarp.com>
2023-06-13 21:29:45 -0300

    added s3 sotrage

--- builtwithdjango/settings.py
+++ builtwithdjango/settings.py
@@ -183,6 +183,14 @@ WEBPACK_LOADER = {
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_S3_REGION_NAME = "eu-east-1"
+AWS_S3_ENDPOINT_URL = env("AWS_S3_ENDPOINT_URL")
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = f"bwd-{ENVIRONMENT}"
+AWS_S3_FILE_OVERWRITE = False
 
 # Sites
 SITE_ID = 1
