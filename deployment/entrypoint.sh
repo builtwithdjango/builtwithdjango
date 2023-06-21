291a89ad4bc7b7246023d1de11e3c866b5038832
Rasul Kireev <rasul.kireev@guycarp.com>
2023-06-21 10:07:06 -0300

    added opentelemetry

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+export DJANGO_SETTINGS_MODULE=builtwithdjango.settings
+
+opentelemetry-bootstrap -a install
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 # python manage.py djstripe_sync_models
 
 python manage.py qcluster &
 
-gunicorn --bind 0.0.0.0:80 --workers 3 builtwithdjango.wsgi:application
+
+
+OTEL_RESOURCE_ATTRIBUTES=service.name=builtwithdjango
+OTEL_EXPORTER_OTLP_ENDPOINT="$SIGNOZ_OTEL_COLLECTOR"
+
+opentelemetry-instrument \
+  --traces_exporter otlp_proto_http \
+  --metrics_exporter otlp_proto_http \
+  gunicorn --bind 0.0.0.0:80 --workers 3 builtwithdjango.wsgi:application
