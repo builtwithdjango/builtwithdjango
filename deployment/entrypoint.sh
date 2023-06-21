660a151327b41d8eab981ae97d1d3249f7de6032
Rasul Kireev <rasul.kireev@guycarp.com>
2023-06-21 10:19:10 -0300

    don't so signoz just yet

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export DJANGO_SETTINGS_MODULE=builtwithdjango.settings
+# export DJANGO_SETTINGS_MODULE=builtwithdjango.settings
 
-opentelemetry-bootstrap -a install
+# opentelemetry-bootstrap -a install
 
 python manage.py collectstatic --noinput
 python manage.py migrate
@@ -12,10 +12,11 @@ python manage.py qcluster &
 
 
 
-OTEL_RESOURCE_ATTRIBUTES=service.name=builtwithdjango
-OTEL_EXPORTER_OTLP_ENDPOINT="$SIGNOZ_OTEL_COLLECTOR"
+# OTEL_RESOURCE_ATTRIBUTES=service.name=builtwithdjango
+# OTEL_EXPORTER_OTLP_ENDPOINT="$SIGNOZ_OTEL_COLLECTOR"
 
-opentelemetry-instrument \
-  --traces_exporter otlp_proto_http \
-  --metrics_exporter otlp_proto_http \
-  gunicorn --bind 0.0.0.0:80 --workers 3 builtwithdjango.wsgi:application
+# opentelemetry-instrument \
+#   --traces_exporter otlp_proto_http \
+#   --metrics_exporter otlp_proto_http \
+
+gunicorn --bind 0.0.0.0:80 --workers 3 builtwithdjango.wsgi:application
