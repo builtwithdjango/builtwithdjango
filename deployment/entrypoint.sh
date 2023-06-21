632137ff0e63bdc2c8b670278933c9ddddc9956b
Rasul Kireev <rasul.kireev@guycarp.com>
2023-06-21 10:50:57 -0300

    try opentelemetry once more

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# export DJANGO_SETTINGS_MODULE=builtwithdjango.settings
-
-# opentelemetry-bootstrap -a install
+opentelemetry-bootstrap -a install
 
 python manage.py collectstatic --noinput
 python manage.py migrate
@@ -10,13 +8,11 @@ python manage.py migrate
 
 python manage.py qcluster &
 
+DJANGO_SETTINGS_MODULE=builtwithdjango.settings
+OTEL_RESOURCE_ATTRIBUTES=service.name=builtwithdjango
+OTEL_EXPORTER_OTLP_ENDPOINT="$SIGNOZ_OTEL_COLLECTOR:4317"
 
-
-# OTEL_RESOURCE_ATTRIBUTES=service.name=builtwithdjango
-# OTEL_EXPORTER_OTLP_ENDPOINT="$SIGNOZ_OTEL_COLLECTOR"
-
-# opentelemetry-instrument \
-#   --traces_exporter otlp_proto_http \
-#   --metrics_exporter otlp_proto_http \
-
-gunicorn --bind 0.0.0.0:80 --workers 3 builtwithdjango.wsgi:application
+opentelemetry-instrument \
+  --traces_exporter otlp_proto_http \
+  --metrics_exporter otlp_proto_http \
+  gunicorn -c gunicorn.config.py --bind 0.0.0.0:80 --workers 3 --threads 2 builtwithdjango.wsgi:application
