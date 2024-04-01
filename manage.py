07c240fead2e28b2476a17a670c35291b8a1dcb3
Rasul Kireev <me@rasulkireev.com>
2024-04-01 09:36:23 +0300

    uncoment instrumentation install

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "builtwithdjango.settings")
