c3751852e487b6a7565241565266970289fa75b3
Rasul Kireev <me@rasulkireev.com>
2024-04-01 17:35:43 +0300

    use http exporter

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -1,7 +1,7 @@
 import os
 
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
