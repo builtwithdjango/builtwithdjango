61cb241a76547a68366164d20a4bc6de6c8ae5f8
Rasul Kireev <me@rasulkireev.com>
2024-04-19 13:19:04 +0300

    improvements

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -1,7 +1,10 @@
 import logging
+from urllib.parse import urlparse
 
+import cloudinary.uploader
 import requests
 from django.conf import settings
+from django.core.files.base import ContentFile
 from django.core.mail import send_mail
 
 from .models import Job
@@ -46,6 +49,8 @@ def get_latest_jobs_from_tj_alerts():
         if Job.objects.filter(external_id=job["id"], source="gettjalerts.com").exists():
             continue
 
+        company_url = job["company_url"]
+
         new_job = Job(
             submitted_datetime=job["submitted_datetime"],
             source="gettjalerts.com",
@@ -59,6 +64,22 @@ def get_latest_jobs_from_tj_alerts():
             location=job["locations"],
             company_name=job["company_name"],
         )
+
+        if company_url:
+            parsed_url = urlparse(company_url)
+            domain_name = parsed_url.netloc
+
+            try:
+
+                image_response = cloudinary.uploader.upload(
+                    f"https://logo.clearbit.com/{domain_name}",
+                    public_id=f"user-profile-image-{settings.ENVIRONMENT}/{domain_name}",
+                )
+                new_job.company_logo = f"image/upload/v{image_response['version']}/{image_response['public_id']}"
+                new_job.approved = True
+            except Exception as e:
+                logger.error(f"Couldn't get company_logo from Clearbit for {domain_name}")
+
         new_job.save()
         count += 1
 
