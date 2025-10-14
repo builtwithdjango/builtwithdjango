ec9a644f997767ad38c7b50798e427d600854044
Rasul Kireev <me@rasulkireev.com>
2025-10-14 17:33:09 +0300

    allauth-fix

--- builtwithdjango/settings.py
+++ builtwithdjango/settings.py
@@ -115,6 +115,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_structlog.middlewares.RequestMiddleware",  # adds log for each request
 ]
